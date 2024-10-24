948159a6ca9049d1dbffc917dbfb8609bf74a0d6
Michael <michaelshalom57@gmail.com>
2024-10-24 02:44:24 -0700

    chatterbox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,57 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    # Handle GET request
+    if request.method == 'GET':
+        ordered_messages = Message.query.order_by(Message.created_at.asc()).all()
+        
+        # Check if the message list is empty
+        if not ordered_messages:
+            return {"message": "No messages found in the database."}, 404
+        else:
+            ordered_messages_dict = [message.to_dict() for message in ordered_messages]
+            return jsonify(ordered_messages_dict), 200
 
-@app.route('/messages/<int:id>')
+    # Handle POST request
+    elif request.method == 'POST':
+        data = request.get_json()
+        body = data.get('body')  
+        username = data.get('username')
+        if not body:
+            return jsonify({'error': 'Message body is required!'}), 400
+        
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        
+        new_message_dict = new_message.to_dict()
+        return jsonify(new_message_dict), 201
+
+# PATCH and DELETE route
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    
+    if message is None:
+        return jsonify({"message": "Message can not be found in the database."}), 404  
+    
+    #  Hanlde PATCH request
+    if request.method == 'PATCH':
+        for attr in request.json:  
+            setattr(message, attr, request.json.get(attr))  
+        db.session.commit()  
+
+        message_dict = message.to_dict()  
+        return jsonify(message_dict), 200 
+
+    # Handle DELETE request
+    elif request.method == 'DELETE':
+        db.session.delete(message)  
+        db.session.commit()  
+        return jsonify({"message": "Message deleted successifuly."}), 204  
+
 
 if __name__ == '__main__':
     app.run(port=5555)
